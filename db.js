51473ce274621c6242300f7db82998d4f6aae490
Chanelle Huang <chanellehuang@gmail.com>
2022-04-10 23:01:09 -1000

    using webpack

--- db.js
+++ db.js
@@ -26,7 +26,11 @@ User.addHook('beforeSave', async (user) => {
 User.byToken = async(token)=> {
   try {
     const payload = jwt.verify(token, process.env.JWT);
-    const user = await User.findByPk(payload.id);
+    const user = await User.findByPk(payload.id, {
+      attributes: {
+        exclude: ['password']
+      }
+    });
     if(user){
       return user;
     }
